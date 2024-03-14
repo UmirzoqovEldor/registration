0af52d184cf2dda04afdb5673730fcd4416a89b3
UmirzoqovEldor <umirzoqoveldor46@gmail.com>
2024-03-14 14:56:36 +0500

    Registration was created

---
+++ src/Root.jsx
@@ -0,0 +1,18 @@
+import First from "./components/first/First"
+import { Container } from "./root"
+
+
+function Root() {
+ 
+
+  return (
+    <>
+    <Container>
+    <First/>
+    </Container>
+     
+    </>
+  )
+}
+
+export default Root
---
+++ src/root.js
@@ -0,0 +1,11 @@
+
+import styled from "styled-components"
+
+
+export const Container =styled("div")`
+margin-left:400px;
+width:400px;
+height:400px;
+
+border:2px solid red;
+`
